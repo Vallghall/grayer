daec7e267dc62335f3226e3af927649b3bad55fb
roman.gusev <roman.gusev@git.media-tel.ru>
2022-12-28 15:38:59 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Vallghall/grayer
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/jpeg"
+	"log"
+	"math"
+	"os"
+	"time"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("expected 1 arg provided, got %d\n", len(os.Args)-1)
+	}
+
+	inputFile, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer inputFile.Close()
+
+	inputImage, err := jpeg.Decode(inputFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	bounds := inputImage.Bounds().Bounds()
+	outputImage := image.NewRGBA(bounds)
+	for i := 0; i < bounds.Dx(); i++ {
+		for j := 0; j < bounds.Dy(); j++ {
+			pixel := inputImage.At(i, j)
+			pc := color.RGBAModel.Convert(pixel).(color.RGBA)
+
+			grey := uint8(math.Round((float64(pc.R) + float64(pc.G) + float64(pc.B)) / 3))
+
+			outputImage.Set(i, j, color.RGBA{
+				R: grey,
+				G: grey,
+				B: grey,
+				A: pc.A,
+			})
+		}
+	}
+
+	outputFile, err := os.Create(fmt.Sprintf("%s.jpeg", time.Now().String()))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer outputFile.Close()
+
+	err = jpeg.Encode(outputFile, outputImage, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
